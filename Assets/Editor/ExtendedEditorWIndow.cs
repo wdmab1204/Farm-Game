80675827f21772ed9090790f08d4c5f32cf7b016
wdmab1204 <wdmab1204@naver.com>
2020-12-13 00:41:40 +0900

    에디터 추가

---
+++ Assets/Editor/ExtendedEditorWIndow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ExtendedEditorWIndow : EditorWindow
+{
+    protected SerializedObject serializedObject;
+    protected SerializedProperty currentProperty;
+
+    protected void DrawProperties(SerializedProperty prop, bool drawChilderen)
+    {
+        string lastPropPath = string.Empty;
+        foreach(SerializedProperty p in prop)
+        {
+            if(p.isArray && p.propertyType == SerializedPropertyType.Generic)
+            {
+                EditorGUILayout.BeginHorizontal();
+                p.isExpanded = EditorGUILayout.Foldout(p.isExpanded, p.displayName);
+                EditorGUILayout.EndHorizontal();
+
+                if (p.isExpanded)
+                {
+                    EditorGUI.indentLevel++;
+                    DrawProperties(p, drawChilderen);
+                    EditorGUI.indentLevel--;
+                }
+            }
+            else
+            {
+                if(!string.IsNullOrEmpty(lastPropPath) && p.propertyPath.Contains(lastPropPath)) { continue; }
+                lastPropPath = p.propertyPath;
+                EditorGUILayout.PropertyField(p, drawChilderen);
+            }
+        }
+    }
+}
---
+++ Assets/Editor/ItemCustomEditor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+public class AssetHandler
+{
+    [OnOpenAsset()]
+    public static bool OpenEditor(int instanceId, int line)
+    {
+        ItemScriptableObject item = EditorUtility.InstanceIDToObject(instanceId) as ItemScriptableObject;
+        if(item != null)
+        {
+            ItemEditorWindow.Open(item);
+            return true;
+        }
+        return false;
+    }
+}
+
+[CustomEditor(typeof(ItemScriptableObject))]
+public class ItemCustomEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        if(GUILayout.Button("Open Editor"))
+        {
+
+        }
+    }
+}
